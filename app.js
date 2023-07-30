389567dced811df33ef44a3ed393aee3f631c420
ggrant111 <57473462+ggrant111@users.noreply.github.com>
2023-07-29 21:02:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ async function joke() {
     let b = await a.json();
     document.getElementById("content").innerHTML = b.joke;
 }
+
+function playRandomSound() {
+    const randomIndex = Math.floor(Math.random() * 4) + 1; // Generate a random number between 1 and 4
+    const sound = document.getElementById(`sound${randomIndex}`);
+    sound.play();
+}
